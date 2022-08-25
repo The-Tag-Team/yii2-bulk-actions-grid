85329f06ab7c1779e44463e8f643300fb2b27523
Nico Ratti <rattinico92@gmail.com>
2022-08-25 10:21:13 +0200

    fix module class

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * @package   yii2-batch-actions-grid
  * @author    Marco Piazza <piazza.m17@gmail.com>
@@ -11,14 +10,6 @@
 
 namespace thetagteam\batchactionsgrid;
 
-use yii\base\InvalidConfigException;
-use yii\base\BootstrapInterface;
-
-/**
- * Base module class for TheTagTeam extensions
- *
- * @author Kartik Visweswaran <kartikv2@gmail.com>
- */
 class Module extends \kartik\base\Module
 {
     /**
